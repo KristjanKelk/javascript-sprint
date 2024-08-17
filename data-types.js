eb7cb2e3cdfb96534e99709fe8459e5fe0d9a3fb
Kristjan Kelk <kristjankelk@gmail.com>
2024-08-17 23:43:16 +0300

    a

--- data-types.js
+++ data-types.js
@@ -1,5 +1,5 @@
-const someBool = new Boolean(Boolean);
+const someBool = Boolean(boolean);
 const someNumber = new Number();
 let someUndefined;
-const someString = new String(String);
+const someString = new String();
 //console.log(someBool, someNumber, someString);

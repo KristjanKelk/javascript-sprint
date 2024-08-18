139fc48bcc3cf68788c2c4b5df237651693e62a5
Kristjan Kelk <kristjankelk@gmail.com>
2024-08-18 10:16:51 +0300

    nested objects

---
+++ nested-object.js
@@ -0,0 +1,13 @@
+let someUndefined
+
+let nestedObj= [{
+  someBool: Boolean(true),
+  someNumber: Number(1),
+  someUndefined,
+  someString: String("String1"),
+}, {
+  someBool: Boolean(false),
+  someNumber: Number(2),
+  someUndefined,
+  someString: String("String2"),
+}];

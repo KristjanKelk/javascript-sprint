d1eefd30ebc594e8f131ccf25528063d18693306
Kristjan Kelk <kristjankelk@gmail.com>
2024-08-23 19:22:00 +0300

    fix

--- diesel.js
+++ diesel.js
@@ -1,37 +1,39 @@
-const carData = '[["Toyota", "Camry", "ABC123", 2014, "diesel"], ["Ford", "Focus", "XYZ456", 1999, "petrol"], ["Peugeot", "206", "XYZ789", 1995, "diesel"], ["BMW", "X5", "ABC456", 2010, "diesel"]]';
-
 function calculateFines(carData) {
+
   const parsedCarData = JSON.parse(carData);
 
-  if (parsedCarData === undefined || parsedCarData.length === 0) {
-    return { totalFines: 0, cars: [] };
-  }
-
   let totalFine = 0;
-  const cars = [];
+  const finedCars = [];
 
   for (let i = 0; i < parsedCarData.length; i++) {
     const car = parsedCarData[i];
+    const make = car[0];
+    const model = car[1];
+    const reg = car[2];
     const year = car[3];
     const fuelType = car[4];
-    const reg = car[2];
 
     let fine = 0;
 
     if (year < 2000) {
       fine = 20;
-    } 
+    }
+
     else if (fuelType === 'diesel' && year < 2015) {
       fine = 10;
     }
 
     if (fine > 0) {
       totalFine += fine;
-      cars.push({ reg, year, fuel: fuelType, fine });
+      finedCars.push({ reg, year, fuelType, fine });
     }
   }
 
-  return { totalFines: totalFine, cars };
+  return JSON.stringify({
+    totalFines: totalFine,
+    cars: finedCars
+  });
 }
 
+const carData = '[["Toyota", "Camry", "ABC123", 2014, "diesel"], ["Ford", "Focus", "XYZ456", 1999, "petrol"]]';
 console.log(calculateFines(carData));

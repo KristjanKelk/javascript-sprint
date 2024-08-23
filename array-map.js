e425c50b4c5e6ffa66aef6f66eb1c3d1ad5d5650
Kristjan Kelk <kristjankelk@gmail.com>
2024-08-23 17:58:16 +0300

    a

--- array-map.js
+++ array-map.js
@@ -2,10 +2,18 @@ function convert2DArrayToObjectArray(arr) {
   return arr.map(([key, value]) => ({ [key]: value }));
 }
 
-function convertArrayOfObjectsToStrings() {
-
+function convertArrayOfObjectsToStrings(objects) {
+  return objects.map(obj => {
+    return `Name: ${obj.name}, Age: ${obj.age}, City: ${obj.city}`;
+  });
 }
 
-function concatenateStrings() {
-
-} 
+function concatenateStrings(strings, maxLength) {
+  return strings.map(str => {
+    if (str.length > maxLength) {
+      return str.slice(0, maxLength) + '...';
+    } else {
+      return str;
+    }
+  });
+}

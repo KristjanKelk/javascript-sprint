8426fa62470fad862d7fc2c93259d1695e868a56
Kristjan Kelk <kristjankelk@gmail.com>
2024-08-23 18:13:14 +0300

    s

--- array-map.js
+++ array-map.js
@@ -2,10 +2,20 @@ function convert2DArrayToObjectArray(arr) {
   return arr.map(([key, value]) => ({ [key]: value }));
 }
 
+const objects = [
+  { name: "Alice", age: 30, city: "New York" },
+  { name: "Bob", age: 25, city: "Los Angeles" },
+]
+
+console.log(convertArrayOfObjectsToStrings(objects))
+
 function convertArrayOfObjectsToStrings(objects) {
   return objects.map(obj => {
-    const entries = Object.keys(obj).map(key => `${key}: ${obj[key]}`);
-    return entries;
+    const entries = Object.keys(obj).map(key => {
+      const capitalizedKey = key.charAt(0).toUpperCase() + key.slice(1);
+      return `${capitalizedKey}: ${obj[key]}`;
+    });
+    return entries.join(', ');
   });
 }
 

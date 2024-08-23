7b9d6c29b6166529e3462c623db36f22b1a3ffb5
Kristjan Kelk <kristjankelk@gmail.com>
2024-08-23 17:53:42 +0300

    as

--- array-map.js
+++ array-map.js
@@ -1,11 +1,5 @@
-const data = [
-  ["name", "Alice"],
-  ["age", 30],
-  ["city", "New York"],
-];
-
 function convert2DArrayToObjectArray(arr) {
-  const reformattedArray = arr.map(({ key, value }) => ({ [key]: value }));
+  return arr.map(([key, value]) => ({ [key]: value }));
 }
 
 function convertArrayOfObjectsToStrings() {

e0c0172a0687af437dc32e9ec9d8c23ec43e4e96
Kristjan Kelk <kristjankelk@gmail.com>
2024-08-23 16:00:04 +0300

    fix

--- math-obj.js
+++ math-obj.js
@@ -1,6 +1,6 @@
 var mathObj = {
   abs(a) {
-    return math.abs(a);
+    return Math.abs(a);
   },
 
   isEven: function(a) {
@@ -9,23 +9,25 @@ var mathObj = {
 
   isOdd: function(a) {
     return (a % 2) != 0;
- },
+  },
 
- isStrictlyPositive: function(a) {
-  return a > 0;
- },
- min(a,b){
-  if (a < b){
-    return a;
-  } else {
-    return b;
+  isStrictlyPositive: function(a) {
+    return a > 0;
+  },
+
+  min(a, b) {
+    if (a < b) {
+      return a;
+    } else {
+      return b;
+    }
+  },
+
+  max(a, b) {
+    if (a > b) {
+      return a;
+    } else {
+      return b;
+    }
   }
- },
- max(a,b){
-  if (a > b){
-    return a;
-  } else {
-    return b;
-  }
- }
 };

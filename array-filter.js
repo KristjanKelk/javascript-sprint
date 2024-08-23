5866c71565e612b3f713992e0c1465fec95962ea
Kristjan Kelk <kristjankelk@gmail.com>
2024-08-23 17:33:23 +0300

    array filter

---
+++ array-filter.js
@@ -0,0 +1,17 @@
+function filterOutOddNumbers(numbers) {
+  return numbers.filter(number => number % 2 === 0);
+}
+
+function filterObjectsByNameLength(str, len) {
+  return str.filter(str.name.length <= len);
+}
+
+function compoundFilter(products) {
+  return products.filter(product => 
+    product.code.length > 5 &&
+    !product.category.includes("special") &&
+    product.price > 50 &&
+    product.location !== "Underground"
+  );
+}
+
